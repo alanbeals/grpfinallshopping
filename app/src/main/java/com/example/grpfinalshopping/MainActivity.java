4fc69ece8223e17dde9c716901ffec5fb3211d39
Patricia Costa <patriciacvcosta@gmail.com>
2022-12-10 19:36:15 -0500

    Add CRUD operations for Order, OrderItem, User and Product

--- app/src/main/java/com/example/grpfinalshopping/MainActivity.java
+++ app/src/main/java/com/example/grpfinalshopping/MainActivity.java
@@ -3,14 +3,37 @@ package com.example.grpfinalshopping;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.grpfinalshopping.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnTest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnTest = findViewById(R.id.btnTest);
+        DbHelper dbHelper = new DbHelper(this);
+//        dbHelper.addUser("Luke Skywalker 2", "111-111-1111", "111 Some Galaxy", "Death Star 2", "BC", "A2A 2A2");
+//
+//        dbHelper.addProduct("Apple", 1);
+//        dbHelper.addProduct("Banana", 2);
+//        dbHelper.addProduct("Strawberry", 5.2);
+//        dbHelper.addProduct("Kiwi", 8.5);
+
+//        dbHelper.updateUserProfile(1,"Luke Skywalker", "111-111-1111", "111 Some Galaxy", "Death Star 2", "BC", "A2A 2A2");
+//
+//        dbHelper.addToCart(1, 1);
+//        dbHelper.addToCart(1, 1);
+//        dbHelper.addToCart(1, 2);
+
+
     }
+
 }
--- app/src/main/java/com/example/grpfinalshopping/SplashActivity.java
+++ app/src/main/java/com/example/grpfinalshopping/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.example.grpfinalshopping;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +11,13 @@ public class SplashActivity  extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent=new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(splashIntent);
+                finish();
+            }
+        },3000);
     }
 }
